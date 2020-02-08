a1ce05029dc11cd06c92a9e556b7bd5072e705ab
Andrew Gentile <agentile1990@gmail.com>
2020-02-08 17:04:02 -0600

    Add CSS SCSS and Style Loaders for Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,17 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             {
                 enforce: "pre",
