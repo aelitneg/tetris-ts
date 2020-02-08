334493dffc4ab2fbf511a9a32118ffd5b6fc067c
Andrew Gentile <agentile1990@gmail.com>
2020-02-08 05:25:06 -0600

    Tweak ESLint Config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
 module.exports = {
     env: {
         browser: true,
-        es6: true
+        es6: true,
     },
     extends: [
         "eslint:recommended",
         "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:prettier/recommended"
+        "plugin:prettier/recommended",
     ],
     globals: {
         Atomics: "readonly",
-        SharedArrayBuffer: "readonly"
+        SharedArrayBuffer: "readonly",
+        module: "readonly",
+        require: "readonly",
+        __dirname: "readonly",
     },
     parser: "@typescript-eslint/parser",
     parserOptions: {
         ecmaVersion: 2018,
-        sourceType: "module"
+        sourceType: "module",
     },
     plugins: ["@typescript-eslint"],
-    rules: {}
+    rules: {},
 };
