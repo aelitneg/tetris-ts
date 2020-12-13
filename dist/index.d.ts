27fced8f92b81ddb2eaca2e5774ee5b1a7762abd
Andrew Gentile <agentile1990@gmail.com>
2020-12-12 22:52:10 -0600

    Module Build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,17 @@
-export {};
+/**
+ * Tetris
+ *
+ * This is the main Tetris class. It connects the main
+ * components, GameEngine, UIEngine, and the EventBus.
+ */
+import { EventBus } from "./lib/EventBus";
+import GameEngine from "./lib/GameEngine";
+import UIEngine from "./lib/UIEngine";
+import { Options } from "./config";
+declare class Tetris {
+    eventBus: EventBus;
+    gameEngine: GameEngine;
+    uiEngine: UIEngine;
+    constructor(rootElement: Element, statsCallback: Function, options?: Options);
+}
+export default Tetris;
